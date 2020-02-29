478f535364d83fcf47708ecb5ef3da7da5d04ab9
andrew <andrew@zust.com>
2020-02-29 23:36:07 +0800

    commit ...

---
+++ src/PushTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author AndrewElvis
+ * @date 2020-02-29-23:35
+ */
+public class PushTest {
+    public static void main(String[] args) {
+        System.out.println("PushTest...");
+    }
+}
