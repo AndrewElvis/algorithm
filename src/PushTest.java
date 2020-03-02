1394c586244241aa074b3db99372efb88c7d58f5
andrew <andrew@zust.com>
2020-03-02 12:27:34 +0800

    commit ...

--- src/PushTest.java
+++ src/PushTest.java
@@ -1,3 +1,5 @@
+import java.util.UUID;
+
 /**
  * @author AndrewElvis
  * @date 2020-02-29-23:35
@@ -6,5 +8,6 @@ public class PushTest {
     public static void main(String[] args) {
         System.out.println("PushTest...");
         System.out.println("Update Project test...");
+        System.out.println(UUID.randomUUID().toString().replace("-","").toUpperCase());
     }
 }
